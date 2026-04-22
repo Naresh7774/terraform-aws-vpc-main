a8331c92f1c367e38a4dc1462e57fd724a5d1b49
Narii Chowdary <nareshchowdary164@gmail.com>
2026-04-22 22:19:15 +0530

    project

--- variables.tf
+++ variables.tf
@@ -38,3 +38,13 @@ variable "private_subnet_cidrs" {
 variable "private_subnet_tags" {
     type = map
     default = {}
+}
+
+variable "database_subnet_cidrs" {
+    type = list
+}
+
+variable "database_subnet_tags" {
+    type = map
+    default = {}
+}
