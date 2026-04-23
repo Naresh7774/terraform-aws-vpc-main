1689fce58011bcb003f1684d9d9ae2a6e64ad9ef
Narii Chowdary <nareshchowdary164@gmail.com>
2026-04-23 23:04:15 +0530

    project

--- vpc.tf
+++ vpc.tf
@@ -171,3 +171,9 @@ resource "aws_route" "database" {
   nat_gateway_id = aws_nat_gateway.nat.id
 }
 
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnet_cidrs)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
