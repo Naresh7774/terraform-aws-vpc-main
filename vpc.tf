ced5020653c149e3e0699689f772aa8c07dea539
Narii Chowdary <nareshchowdary164@gmail.com>
2026-04-23 23:04:00 +0530

    project

--- vpc.tf
+++ vpc.tf
@@ -157,3 +157,9 @@ resource "aws_nat_gateway" "nat" {
   depends_on = [aws_internet_gateway.main]
 }
 
+# Private egress route through NAT
+resource "aws_route" "private" {
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.nat.id
+}
