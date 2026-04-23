f8f21e088958f7275bd25323ec0b318863a78770
Narii Chowdary <nareshchowdary164@gmail.com>
2026-04-23 23:00:51 +0530

    project

--- vpc.tf
+++ vpc.tf
@@ -4,3 +4,11 @@ resource "aws_vpc" "main" {
   instance_tenancy = "default"
   enable_dns_hostnames = true
 
+  tags = merge(
+    var.vpc_tags,
+    local.common_tags,
+    {
+        Name = local.common_name_suffix
+    }
+  )
+}
