60a2f56da6a4605114a6a1a527497b06df927ec1
Narii Chowdary <nareshchowdary164@gmail.com>
2026-04-23 23:04:22 +0530

    project

--- vpc.tf
+++ vpc.tf
@@ -177,3 +177,9 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 }
 
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnet_cidrs)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
